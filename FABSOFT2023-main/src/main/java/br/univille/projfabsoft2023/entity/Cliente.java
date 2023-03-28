74c6b488e87c8399ccb55ba4f5d08013f3e7fd52
MARCOS ENRICO DAMAZZINI GODINHO <marcos.godinho@univille.br>
2023-03-27 22:24:57 -0300

    Mudei

--- FABSOFT2023-main/src/main/java/br/univille/projfabsoft2023/entity/Cliente.java
+++ FABSOFT2023-main/src/main/java/br/univille/projfabsoft2023/entity/Cliente.java
@@ -2,9 +2,22 @@ package br.univille.projfabsoft2023.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(length = 600)
     private String nome;
+    @Temporal(value = TemporalType.DATE)
     private Date dataNascimento;
 
     public long getId() {
